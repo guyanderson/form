5e20af5ac284c4430dad7514f6000fcd962e3844
guy <luckythelich@hotmail.com>
2017-10-23 18:36:06 -0700

    blah

---
+++ src/CampingForm/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CampingForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Boot()
+        {
+            return View();
+        }
+
+        public IActionResult duplicateEntryWarning(name)
+        {
+            public List<string> campNames = new List<string> { "Skokomish Park", "Silver Falls" };
+
+            for (let i = 0; i < campNames.length; i++)
+            {
+                if (campNames[i].toLowerCase().includes(name.toLowerCase())
+                {
+                    alert("please check that the " name " campsite has not already been entered.");
+                }
+                else 
+                {
+                    alert("looks good");
+                }
+            }
+        }
+        
+    }
+}
+
+showCharityByName(searchTerm)
+{
+
+    this.charities = [];
+
+    this.database.getCharities().subscribe(response => {
+
+        for (let i = 0; i < response.length; i++)
+        {
+
+            if (response[i].charityName.toLowerCase().includes(searchTerm.toLowerCase()))
+            {
+
+                this.charities.push(response[i]);
+
+            }
+
+        }
+
+    });
+
+}
---
+++ src/CampingForm/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace CampingForm
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddEntityFramework();
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
